26c7616909eea42b919447eb94bcf3409b5cee8b
kyungsubbb <48882962+kyungsubbb@users.noreply.github.com>
2019-08-10 03:44:29 +0900

    Update models.py

--- room/models.py
+++ room/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 134c83ce8c0895bebd16f54ec13d3f1f39921512
 class Room(models.Model):
     ROOM_CHOICE = {
         ('원룸','원룸'),
@@ -17,10 +13,7 @@ class Room(models.Model):
     title = models.CharField(max_length=300)
     roomtype = models.CharField(max_length=100, choices=ROOM_CHOICE)
     price = models.CharField(max_length=200)
-<<<<<<< HEAD
     pub_date = models.DateTimeField('date_published', null=True)
-=======
->>>>>>> 134c83ce8c0895bebd16f54ec13d3f1f39921512
     start_date = models.DateField()
     end_date = models.DateField()
     phonenumber = models.IntegerField()
@@ -29,15 +22,8 @@ class Room(models.Model):
     img1 = models.ImageField(upload_to='images/', blank=True)
     img2 = models.ImageField(upload_to='images/', blank=True)
     img3 = models.ImageField(upload_to='images/', blank=True)
-<<<<<<< HEAD
     sample5_address = models.CharField(max_length=300,null=True)
 
 
     def __str__(self):
         return self.title
-=======
-
-    def __str__(self):
-        return self.title
-
->>>>>>> 134c83ce8c0895bebd16f54ec13d3f1f39921512
